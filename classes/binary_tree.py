06d5b66fa862264e210df8e04308428de28e3ee3
desracto <theminer123321@gmail.com>
2023-07-12 18:02:39 +0400

    Added Images

--- classes/binary_tree.py
+++ classes/binary_tree.py
@@ -1,10 +1,27 @@
+import sqlparse
+
 # The formula to access parent node and it's leaves is:
 #       i: parent
-#     i*2: left
-#   i*2+1: right
+#    2i+1: left
+#    2i+2: right
+
+# To do:
+# Write down rules for converting sql query to relational algebra tree
+# Order of building tree from the bottom:
+# FROM
+# WHERE
+# JOIN
+# ORDER
+# 
+
+# SELECT
+
+class dataDictionary:
+    
 
 class Node:
-    def __init__(self, value):
+    def __init__(self, keyword: str, value):
+        self.keyword = keyword
         self.value = value
 
 class BinaryTree:
@@ -12,8 +29,17 @@ class BinaryTree:
         self.tree: list[Node] = []
     
     def insertNode(self, node: Node):
+        """ 
+            This function takes a set of nodes and develops the tree based on Relational Algebra Tree rules
+        """
         self.tree.append(node)
     
+    def printTree(self):
+        return ""
     
+
+if __name__ == "__main__":
+    sql_query = "SELECT * FROM customers WHERE id = 500"
+    print(sqlparse.parse(sql_query)[0][2])
 
     
