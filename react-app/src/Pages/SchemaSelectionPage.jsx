24eaded1d58e4d162ff74160003025d15e9fef69
Hiba Gohar <78848419+hibaagohar@users.noreply.github.com>
2023-09-14 18:32:13 +0400

    Editied Shema Selection Files (Ignore the placememt of SELECT Button)

--- react-app/src/Pages/SchemaSelectionPage.jsx
+++ react-app/src/Pages/SchemaSelectionPage.jsx
@@ -6,24 +6,25 @@ export default function SchemaSelectionPage() {
     <>
       <Header1 />
       <div className="SchemaSelectionContainer">
-        <div className="SchemaSelectionCard">
-          {/* Figma components */}
-          <div className="SchemaSelectionTitle">Schema Selection</div>
-          <div className="SchemaSelectionText">
-            Begin your journey into the world of data manipulation and SQL query visualization by selecting from our five diverse range of pre-designed schemas.
-          </div>
-          <div className="colored-line" /> {/* Colored line */}
-          <div className="button-container">
-            <button className="Schema1Button">SCHEMA 1</button>
-            <button className="Schema2Button">SCHEMA 2</button>
-            <button className="Schema3Button">SCHEMA 3</button>
-            <button className="Schema4Button">SCHEMA 4</button>
-            <button className="Schema5Button">SCHEMA 5</button>
-        </div>
-        
-        <div className="SchemaDisplayCard">
-        </div>
-        </div>
+            <div className="SchemaSelectionCard">
+                <div className="SchemaSelectionTitle">Schema Selection</div>
+                <div className="SchemaSelectionText">
+                Begin your journey into the world of data manipulation and SQL query visualization by selecting from our five diverse range of pre-designed schemas.
+                </div>
+                <div className="colored-line" /> {/* Colored line */}
+                <div className="button-container">
+                    <button className="Schema1Button">SCHEMA 1</button>
+                    <button className="Schema2Button">SCHEMA 2</button>
+                    <button className="Schema3Button">SCHEMA 3</button>
+                    <button className="Schema4Button">SCHEMA 4</button>
+                    <button className="Schema5Button">SCHEMA 5</button>
+                </div>
+                <div className="SchemaDisplayCard">
+                </div>
+                <div className="SchemaSelectButton-container">
+                    <button className="SchemaSelectButton">SELECT</button>
+                </div>
+            </div>
       </div>
     </>
   );
