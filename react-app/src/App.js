f03119f1125b4b4d93309b31594dc7355578fd3a
Hiba Gohar <78848419+hibaagohar@users.noreply.github.com>
2023-09-14 16:52:40 +0400

    Edited SchemaSelectionPage and Added font folder and files

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import LandingPage from './Pages/LandingPage'
+import SchemaSelectionPage from './Pages/SchemaSelectionPage'
 import NoPage from './Pages/NoPage'
-
  
 function App() {
   return (
@@ -11,6 +11,7 @@ function App() {
         <Routes>
           <Route index element={<LandingPage />} /> {/* Root URL when the app is accessed*/}
           <Route path="/LandingPage" element={<LandingPage />} /> {/* To make sure this path is also valid, without the root path*/}
+          <Route path="/SchemaSelectionPage" element={<SchemaSelectionPage />} /> 
           <Route path="*" element={<NoPage />} /> {/*Anything other than what is already defined*/}
 
         </Routes>
--- react-app/src/Pages/SchemaSelectionPage.jsx
+++ react-app/src/Pages/SchemaSelectionPage.jsx
@@ -1,10 +1,21 @@
-import Header1 from "../components/Header1"
+import Header1 from "../components/Header1";
+import "./SchemaSelectionPage.css";
 
-export default function SchemaSelectionPage() 
-{
-    return (
-        <>
-            <Header1 />
-        </>
-    )
+export default function SchemaSelectionPage() {
+  return (
+    <>
+      <Header1 />
+      <div className="SchemaSelectionContainer">
+        <div className="SchemaSelectionCard">
+          {/* Figma components */}
+          <div className="SchemaSelectionTitle">Schema Selection</div>
+          <div className="SchemaSelectionText">
+            Begin your journey into the world of data manipulation and SQL query visualization by selecting from our five diverse range of pre-designed schemas.
+          </div>
+          <div className="colored-line" /> {/* Colored line */}
+          <button className="Schema1Button">Schema 1</button>
+        </div>
+      </div>
+    </>
+  );
 }
