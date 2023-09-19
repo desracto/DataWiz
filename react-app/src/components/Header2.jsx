6ea38d7cdb6c2097d59527b2fa95f913a0cece25
Hiba Gohar <78848419+hibaagohar@users.noreply.github.com>
2023-09-19 08:21:21 +0400

    Adjusted Navbar in Header2

--- react-app/src/components/Header2.jsx
+++ react-app/src/components/Header2.jsx
@@ -4,53 +4,66 @@ export default function Header2()
 {
     return (
         <div className="flex">
-            <div className="Ellipse">
-            <img
-                src={process.env.PUBLIC_URL + '/images/DataWiz-Logo.png'}
-                alt="DataWiz Logo"
-                style={{ maxWidth: '70%', maxHeight: '70%' }} // Adjust the values as needed
-            />
+            <div className="LogoBrandContainer">
+                <div className="Ellipse">
+                <img
+                    src={process.env.PUBLIC_URL + '/images/DataWiz-Logo.png'}
+                    alt="DataWiz Logo"
+                    style={{ maxWidth: '70%', maxHeight: '70%' }} // Adjust the values as needed
+                />
+                </div>
+                <div className="Datawiz w-96 h-28">
+                <span style={{ color: 'white', fontSize: '10rem', fontWeight: 'bold' }}>Data</span>
+                <span style={{ color: 'white', fontSize: '10rem', fontWeight: 'normal' }}>Wiz.</span>
+                </div>
             </div>
-            <div className="Datawiz w-96 h-28">
-            <span style={{ color: 'white', fontSize: '10rem', fontWeight: 'bold' }}>Data</span>
-            <span style={{ color: 'white', fontSize: '10rem', fontWeight: 'normal' }}>Wiz.</span>
-            </div>
+
             <div className="NavBarContainer">
                 <div className="NavBarCard">
-                    <img
+                    <div className="buttonContainer">
+                        <img
                         src={process.env.PUBLIC_URL + '/images/HomeIcon.png'}
                         alt="Home Icon"
-                        style={{ maxWidth: '150px', maxHeight: '150px' }} 
-                    />
-                    <button className="NavBar-HOME-Button">Home</button>
+                        className="iconImage"
+                        />
+                        <button className="NavBar-HOME-Button">Home</button>
+                    </div>
 
-                    <img
+                    <div className="buttonContainer">
+                        <img
                         src={process.env.PUBLIC_URL + '/images/AnimationIcon.png'}
                         alt="Animation Icon"
-                        style={{ maxWidth: '150px', maxHeight: '150px' }} 
-                    />
-                    <button className="NavBar-ANI-Button">Animation</button>
+                        className="iconImage"
+                        />
+                        <button className="NavBar-ANI-Button">Animation</button>
+                    </div>
 
-                    <img
+                    <div className="buttonContainer">
+                        <img
                         src={process.env.PUBLIC_URL + '/images/QuizzesIcon.png'}
                         alt="Quizzes Icon"
-                        style={{ maxWidth: '150px', maxHeight: '150px' }} 
-                    />
-                    <button className="NavBar-QUIZZES-Button">Quizzes</button>
-                    
-                    <img
+                        className="iconImage"
+                        />
+                        <button className="NavBar-QUIZZES-Button">Quizzes</button>
+                    </div>
+
+                    <div className="buttonContainer">
+                        <img
                         src={process.env.PUBLIC_URL + '/images/FAQsIcon.png'}
                         alt="Faqs Icon"
-                        style={{ maxWidth: '150px', maxHeight: '150px' }} 
-                    />
-                    <button className="NavBar-FAQS-Button">FAQs</button>
+                        className="iconImage"
+                        />
+                        <button className="NavBar-FAQS-Button">FAQs</button>
+                    </div>
 
-                    <img
+                    <div className="buttonContainer">
+                        <img
                         src={process.env.PUBLIC_URL + '/images/SettingsIcon.png'}
                         alt="Settings Icon"
-                        style={{ maxWidth: '150px', maxHeight: '150px' }} // Adjust the values as needed
-                    />
-                    <button className="NavBar-SETTINGS-Button">Settings</button>
+                        className="iconImage"
+                        />
+                        <button className="NavBar-SETTINGS-Button">Settings</button>
+                    </div>
                 </div>
             </div>
         </div>
