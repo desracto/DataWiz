44991bc687c9d3a840770f008cc5ee50a38d6293
M.Nihal <mnk665@uowmail.edu.au>
2023-09-24 14:08:02 +0400

    finished signup page

--- react-app/src/components/FormInputs.jsx
+++ react-app/src/components/FormInputs.jsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import "./forminputs.css";
 
 const FormInputs = (props) => {
-  const { label, onChange, id, ...inputProps } = props;
+  const { label, errorMessage, onChange, id, ...inputProps } = props;
+  const [focused, setFocused] = useState(false);
+  
+  const handleFocus = (e) => {
+    setFocused(true);
+  };
+
   return (
     <div className="FormInputs">
-      <label>{label}</label> <br />
-      <input {...inputProps} onChange={onChange} />
+      <label>{label}</label>
+      <input
+        {...inputProps}
+        onChange={onChange}
+        onBlur={handleFocus}
+        focused={focused.toString()}
+      />
+      <span className = "ErrorMessage">{errorMessage}</span>
     </div>
   );
 };
