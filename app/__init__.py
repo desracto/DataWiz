4c71d00145be5d1c90bc2931b7229e65146a0e57
desracto <mohammedejazzur@gmail.com>
2023-10-01 16:24:11 +0400

    Changes: - Fixed bug

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,7 @@ from flask import Flask
 from config import Config
 from .extensions import db, migrate, jwt, cors
 
-app = Flask(__name__)
-app.config.from_object(Config)
-
-def create_app(config_class=Config):
+def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
 
@@ -15,23 +12,23 @@ def create_app(config_class=Config):
     cors.init_app(app, supports_credentials=True)
     
     # Main blueprint
-    from .blueprints.main import main_bp
+    from app.blueprints.main import main_bp
     app.register_blueprint(main_bp)
 
     # Auth Blueprint
-    from .blueprints.auth import auth_bp
+    from app.blueprints.auth import auth_bp
     app.register_blueprint(auth_bp, url_prefix="/api/auth/")
 
     # User Blueprint
-    from .blueprints.user import user_bp
+    from app.blueprints.user import user_bp
     app.register_blueprint(user_bp, url_prefix="/api/user/")
 
     # Animation Blueprint
-    from .blueprints.animation import animation_bp 
+    from app.blueprints.animation import animation_bp 
     app.register_blueprint(animation_bp, url_prefix="/api/animation/")
 
     # Quiz Blueprint
-    from .blueprints.quiz import quiz_bp
+    from app.blueprints.quiz import quiz_bp
     app.register_blueprint(quiz_bp, url_prefix="/api/quiz/")
 
     return app
